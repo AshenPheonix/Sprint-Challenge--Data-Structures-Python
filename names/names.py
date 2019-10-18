b8514b5fb985923849572b317c13df338c41e4d9
AshenPheonix <brendlorend@gmail.com>
2019-10-18 08:30:11 -0700

    WIP Names to .19 second

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -6,15 +7,27 @@ f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+BST = BinarySearchTree(names_1[0])
+names_1 = names_1[1:]
+
+for name in names_1:
+    BST.insert(name)
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+for name in names_2:
+    BST.insert(name)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+BST.get_copies(duplicates)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
